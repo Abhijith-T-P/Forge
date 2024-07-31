357dc96fcce90c1c1f40ca0f8938a85552104252
Abhijith <abhijithtp2003@gmail.com>
2024-07-31 21:29:17 +0530

    Sales sidebar updated

--- src/Sales/Components/Sidebar/Sidebar.jsx
+++ src/Sales/Components/Sidebar/Sidebar.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import './Sidebar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Sidebar = ({ isSidebarOpen, toggleSidebar }) => (
-  <div className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
-    <nav>
-      <ul>
-        <li>
-          <Link  onClick={toggleSidebar}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="var(--text-color)">
-              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-            </svg>
-          </Link>
-        </li>
-        <li><Link to='./' onClick={toggleSidebar}>Dashboard</Link></li>
-        <li><Link to="./Stock/Finished" onClick={toggleSidebar}>StockList</Link></li>
-       
-         
-      </ul>
-    </nav>
-  </div>
-);
+const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('user'); // Clear user data from local storage
+    navigate('/'); // Redirect to login or home page
+  };
+
+  return (
+    <div className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
+      <nav>
+        <ul>
+          <li>
+            <Link onClick={toggleSidebar}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="var(--text-color)">
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+              </svg>
+            </Link>
+          </li>
+          <li><Link to='./' onClick={toggleSidebar}>Dashboard</Link></li>
+          <li><Link to="./Stock/Finished" onClick={toggleSidebar}>StockList</Link></li>
+          <li><Link to="/" onClick={handleLogout}>LogOut</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Sidebar;
